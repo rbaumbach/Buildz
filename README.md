b5064a9e55d25ce4dda5219fe43f71748dbfd001
Ryan <github@ryan.codes>
2020-01-11 16:34:45 -0700

    Add simple project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Buildz
 
 A repo to practice building apps manually...
+
+# Using XcodeBuild
---
+++ Simple/SimpleTests/SimpleTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Simple
+
+class SimpleTests: XCTestCase {
+    override func setUp() { }
+
+    override func tearDown() { }
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
